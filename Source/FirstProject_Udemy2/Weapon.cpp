45330744f45c20b5d30ce814a96d2d404e12b0ee
shea <galley.shea@gmail.com>
2021-03-02 15:51:51 -0700

    59 | Created VS files for Weapons inherited from class Item

---
+++ Source/FirstProject_Udemy2/Weapon.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon.h"
+#include "Components/SkeletalMeshComponent.h"
+
+AWeapon::AWeapon()
+{
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMesh->SetupAttachment(GetRootComponent());
+}
+
+void AWeapon::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+void AWeapon::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
---
+++ Source/FirstProject_Udemy2/Weapon.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTPROJECT_UDEMY2_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+public:
+
+	AWeapon();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "SkeletalMesh")
+	class USkeletalMeshComponent* SkeletalMesh;
+
+	//Inherited function cannot be marked UFUNCTION.  We use "override" to overwrite the properties of the parent function
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+	
+};
